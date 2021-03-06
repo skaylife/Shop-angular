09f311ee2fcaa239c12b55d4272da2835e9763d3
unknown <worldskaynet@gmail.com>
2021-03-06 20:32:44 +0700

    Add Menu Users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '', component: MainPageComponent },
+      { path: 'product/:id', component: ProductPageComponent },
+      { path: 'cart', component: CartPageComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
