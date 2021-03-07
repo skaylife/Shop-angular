634cf1adfe373e4a71ec24c11db32542fbaa5b4a
unknown <worldskaynet@gmail.com>
2021-03-07 16:14:25 +0700

    Add time life auth token & func Logic logout

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +9,42 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  login( User ) {
-    return this.http.post(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, User
-    );
+  login(User) {
+    return this.http
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,
+        User
+      )
+      .pipe(tap(this.setToken));
+  }
+
+  private setToken(response) {
+    if (response) {
+      const expData = new Date(
+        new Date().getTime() + +response.expiresIn * 1000
+      );
+      localStorage.setItem('fb-token-exp', expData.toString()),
+        localStorage.setItem('fb-token', response.idToken);
+    } else {
+      localStorage.clear();
+    }
+  }
+
+  get token() {
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
+  }
+
+  logout() {
+    this.setToken(null);
+  }
+
+  isAuth() {
+    return !!this.token; //Если токен null, то происходит конверт null в - False | True = True/ Если False значит user не авторизован
   }
 }
 
