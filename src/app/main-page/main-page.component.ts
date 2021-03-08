faebae1642b002bab3c69f9f2466475dc6c3ce02
unknown <worldskaynet@gmail.com>
2021-03-08 14:49:20 +0700

    Add product page 5.2 last 3,4

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -4,13 +4,19 @@ import { ProductService } from '../shared/product.service';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss'],
+  styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
-  products$;
-  constructor(private productServ: ProductService) {}
+
+
+  products$ 
+
+  constructor(
+    private productServ: ProductService
+  ) { }
 
   ngOnInit() {
-    this.products$ = this.productServ.getAll();
+    this.products$ = this.productServ.getAll()
   }
+
 }
--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,36 +1,47 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { FbResponse } from './interfaces';
+import { map } from 'rxjs/operators';
+import { FbResponse, Product } from './interfaces';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ProductService {
-  constructor(private http: HttpClient) {}
+
+  constructor(private  http : HttpClient) { }
 
   create(product) {
-    return this.http.post(`${environment.fbDbUrl}/products.json`, product).pipe(
-      map((res: FbResponse) => {
-        return {
-          product,
-          id: res.name,
-          date: new Date(product.date),
-        };
-      })
-    );
+    return this.http.post(`${environment.fbDbUrl}/products.json`, product)
+    .pipe(map( (res : FbResponse) => {
+      return {
+        ...product,
+        id: res.name,
+        date: new Date(product.date)
+      }
+    }))
   }
 
   getAll() {
-    return this.http.get(`${environment.fbDbUrl}/products.json`).pipe(
-      map((res) => {
-        return Object.keys(res).map((key) => ({
-          ...res[key],
-          id: key,
-          date: new Date(res[key].date),
-        }));
-      })
-    );
+    return this.http.get(`${environment.fbDbUrl}/products.json`)
+    .pipe( map ( res => {
+      return Object.keys(res)
+      .map( key => ({
+        ...res[key],
+        id: key,
+        date: new Date(res[key].date)
+      }))
+    }))
+  }
+
+  getById(id) {
+    return this.http.get(`${environment.fbDbUrl}/products/${id}.json`)
+    .pipe( map ( (res: Product) => {
+      return {
+        ...res,
+        id,
+        date: new Date(res.date)
+      }
+    }))
   }
 }
